f533dfa2bb9e4ed90e5bc3ad3c5622103e8b9009
Andrea Tang <andreamtang@gmail.com>
2021-09-20 11:19:32 -0700

    feat(APOD): Add button that fetches additional images to display on feed

--- src/components/ImageCardList/ImageCardList.js
+++ src/components/ImageCardList/ImageCardList.js
@@ -2,40 +2,40 @@ import React from 'react';
 import ImageCard from '../ImageCard/ImageCard.js';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Col, Row } from 'react-bootstrap';
+import { Container, Col, Row, Button } from 'react-bootstrap';
+import './ImageCardList.css';
 
 const API_KEY = '49gRciJcK9SCUEdVg3J4exN2A6cIo1pfM6Hs7zjL'
 const IMAGE_COUNT = 6
 
 class ImageCardList extends React.Component {
   state = {
-    data: []
+    images: []
   }
 
   componentDidMount() {
     this.fetchImages();
   }
 
-  fetchImages() {
-    axios
-      .get(`https://api.nasa.gov/planetary/apod?count=${IMAGE_COUNT}&api_key=${API_KEY}`)
-      .then(res => {
-        const data = res.data
-        this.setState({ data });
+  fetchImages = async () => {
+    try {
+      const res = await axios.get(`https://api.nasa.gov/planetary/apod?count=${IMAGE_COUNT}&api_key=${API_KEY}`);
+      const data = res.data;
+      this.setState({ images: [...this.state.images, ...data] });
 
-        console.log(data);
-      })
-      .catch(error => {
-        console.log(error);
-        this.setState({ data: [] });
-      });
+      console.log(data);
+    }
+    catch (error) {
+      console.error(error);
+      this.setState({ images: this.state.images });
+    }
   }
 
   render() {
     return (
-      <Container className="mt-5">
+      <Container className="pt-5 pb-5">
         <Row xs={1} sm={1} md={2} lg={3}>
-          {this.state.data.map(imageData => ( 
+          {this.state.images.map(imageData => ( 
             imageData.media_type === "image" && (
               <Col key={imageData.url}>
                 <ImageCard
@@ -48,6 +48,9 @@ class ImageCardList extends React.Component {
             )
           ))}
         </Row>
+        <div className="text-center">
+          <Button variant="outline-light" className="Button" onClick={this.fetchImages}>Load More</Button>
+        </div>
       </Container>
     );
   }
